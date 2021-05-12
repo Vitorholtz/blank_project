75ee704ee24d16e5c5c2853d026b43db110991d6
Vitor  Holtz <vitorholtz27@gmail.com>
2021-05-11 21:50:39 -0300

    yu

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-on window scroll {
-    if (scroll pos > 100) {
-      element.addclass("animateMe");
-    }
-  }
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  let position = 600;
+  let position2 = 2600;
+
+  if (currentScrollPos >= position) {
+      document.querySelector(".content__container").style ='display:flex;';
+  } 
+
+  if (currentScrollPos >= position2) {
+    document.querySelector(".instructions__container").style ='display:flex;';
+} 
+}
